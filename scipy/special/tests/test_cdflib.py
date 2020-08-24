2054591e0b8c369d26ce6d939823cc21622da05c
Samuel St-Jean <samuel@isi.uu.nl>
2020-08-24 10:37:38 -0400

    use rtol=1e-12 because of 32 bit version

--- scipy/special/tests/test_cdflib.py
+++ scipy/special/tests/test_cdflib.py
@@ -213,14 +213,14 @@ class TestCDFlib(object):
             sp.bdtrik,
             _binomial_cdf,
             0, [ProbArg(), IntArg(1, 1000), ProbArg()],
-            rtol=1e-15)
+            rtol=1e-12)
 
     def test_bdtrin(self):
         _assert_inverts(
             sp.bdtrin,
             _binomial_cdf,
             1, [IntArg(1, 1000), ProbArg(), ProbArg()],
-            rtol=1e-15, endpt_atol=[None, None, 1e-6])
+            rtol=1e-12, endpt_atol=[None, None, 1e-6])
 
     def test_btdtria(self):
         _assert_inverts(
@@ -228,7 +228,7 @@ class TestCDFlib(object):
             lambda a, b, x: mpmath.betainc(a, b, x2=x, regularized=True),
             0, [ProbArg(), Arg(0, 1e2, inclusive_a=False),
                 Arg(0, 1, inclusive_a=False, inclusive_b=False)],
-            rtol=1e-15)
+            rtol=1e-12)
 
     def test_btdtrib(self):
         # Use small values of a or mpmath doesn't converge
@@ -237,7 +237,7 @@ class TestCDFlib(object):
             lambda a, b, x: mpmath.betainc(a, b, x2=x, regularized=True),
             1, [Arg(0, 1e2, inclusive_a=False), ProbArg(),
              Arg(0, 1, inclusive_a=False, inclusive_b=False)],
-            rtol=1e-15, endpt_atol=[None, 1e-18, 1e-15])
+            rtol=1e-12, endpt_atol=[None, 1e-18, 1e-15])
 
     @pytest.mark.xfail(run=False)
     def test_fdtridfd(self):
@@ -245,14 +245,14 @@ class TestCDFlib(object):
             sp.fdtridfd,
             _f_cdf,
             1, [IntArg(1, 100), ProbArg(), Arg(0, 100, inclusive_a=False)],
-            rtol=1e-15)
+            rtol=1e-12)
 
     def test_gdtria(self):
         _assert_inverts(
             sp.gdtria,
             lambda a, b, x: mpmath.gammainc(b, b=a*x, regularized=True),
             0, [ProbArg(), Arg(0, 1e3, inclusive_a=False),
-                Arg(0, 1e4, inclusive_a=False)], rtol=1e-15,
+                Arg(0, 1e4, inclusive_a=False)], rtol=1e-12,
             endpt_atol=[None, 1e-7, 1e-10])
 
     def test_gdtrib(self):
@@ -261,14 +261,14 @@ class TestCDFlib(object):
             sp.gdtrib,
             lambda a, b, x: mpmath.gammainc(b, b=a*x, regularized=True),
             1, [Arg(0, 1e2, inclusive_a=False), ProbArg(),
-                Arg(0, 1e3, inclusive_a=False)], rtol=1e-15)
+                Arg(0, 1e3, inclusive_a=False)], rtol=1e-12)
 
     def test_gdtrix(self):
         _assert_inverts(
             sp.gdtrix,
             lambda a, b, x: mpmath.gammainc(b, b=a*x, regularized=True),
             2, [Arg(0, 1e3, inclusive_a=False), Arg(0, 1e3, inclusive_a=False),
-                ProbArg()], rtol=1e-15,
+                ProbArg()], rtol=1e-12,
             endpt_atol=[None, 1e-7, 1e-10])
 
     def test_stdtr(self):
@@ -276,27 +276,27 @@ class TestCDFlib(object):
         assert_mpmath_equal(
             sp.stdtr,
             _student_t_cdf,
-            [IntArg(1, 100), Arg(1e-10, np.inf)], rtol=1e-15)
+            [IntArg(1, 100), Arg(1e-10, np.inf)], rtol=1e-12)
 
     @pytest.mark.xfail(run=False)
     def test_stdtridf(self):
         _assert_inverts(
             sp.stdtridf,
             _student_t_cdf,
-            0, [ProbArg(), Arg()], rtol=1e-15)
+            0, [ProbArg(), Arg()], rtol=1e-12)
 
     def test_stdtrit(self):
         _assert_inverts(
             sp.stdtrit,
             _student_t_cdf,
-            1, [IntArg(1, 100), ProbArg()], rtol=1e-15,
+            1, [IntArg(1, 100), ProbArg()], rtol=1e-12,
             endpt_atol=[None, 1e-10])
 
     def test_chdtriv(self):
         _assert_inverts(
             sp.chdtriv,
             lambda v, x: mpmath.gammainc(v/2, b=x/2, regularized=True),
-            0, [ProbArg(), IntArg(1, 100)], rtol=1e-15)
+            0, [ProbArg(), IntArg(1, 100)], rtol=1e-12)
 
     @pytest.mark.xfail(run=False)
     def test_chndtridf(self):
@@ -306,7 +306,7 @@ class TestCDFlib(object):
             _noncentral_chi_cdf,
             1, [Arg(0, 100, inclusive_a=False), ProbArg(),
                 Arg(0, 100, inclusive_a=False)],
-            n=1000, rtol=1e-15, atol=1e-15)
+            n=1000, rtol=1e-12, atol=1e-12)
 
     @pytest.mark.xfail(run=False)
     def test_chndtrinc(self):
@@ -315,7 +315,7 @@ class TestCDFlib(object):
             sp.chndtrinc,
             _noncentral_chi_cdf,
             2, [Arg(0, 100, inclusive_a=False), IntArg(1, 100), ProbArg()],
-            n=1000, rtol=1e-15, atol=1e-15)
+            n=1000, rtol=1e-12, atol=1e-12)
 
     def test_chndtrix(self):
         # Use a larger atol since mpmath is doing numerical integration
@@ -323,7 +323,7 @@ class TestCDFlib(object):
             sp.chndtrix,
             _noncentral_chi_cdf,
             0, [ProbArg(), IntArg(1, 100), Arg(0, 100, inclusive_a=False)],
-            n=1000, rtol=1e-15, atol=1e-15,
+            n=1000, rtol=1e-12, atol=1e-12,
             endpt_atol=[1e-6, None, None])
 
     def test_chndtr(self):
@@ -332,7 +332,7 @@ class TestCDFlib(object):
             sp.chndtr,
             _noncentral_chi_cdf,
             0, [ProbArg(), IntArg(1, 100), Arg(0, 100, inclusive_a=False)],
-            n=1000, rtol=1e-15, atol=1e-15,
+            n=1000, rtol=1e-12, atol=1e-12,
             endpt_atol=[1e-6, None, None])
 
     def test_tklmbda_zero_shape(self):
@@ -341,14 +341,14 @@ class TestCDFlib(object):
         assert_mpmath_equal(
             lambda x: sp.tklmbda(x, 0),
             lambda x: one/(mpmath.exp(-x) + one),
-            [Arg()], rtol=1e-15)
+            [Arg()], rtol=1e-12)
 
     def test_tklmbda_neg_shape(self):
         _assert_inverts(
             sp.tklmbda,
             _tukey_lmbda_quantile,
             0, [ProbArg(), Arg(-25, 0, inclusive_b=False)],
-            spfunc_first=False, rtol=1e-15,
+            spfunc_first=False, rtol=1e-12,
             endpt_atol=[1e-9, 1e-5])
 
     @pytest.mark.xfail(run=False)
@@ -357,7 +357,7 @@ class TestCDFlib(object):
             sp.tklmbda,
             _tukey_lmbda_quantile,
             0, [ProbArg(), Arg(0, 100, inclusive_a=False)],
-            spfunc_first=False, rtol=1e-15)
+            spfunc_first=False, rtol=1e-12)
 
 
 def test_nonfinite():
